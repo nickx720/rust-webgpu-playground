03fdb472e97684701585cc8bc9dfaf883d7355f7
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-04-02 12:00:38 -0400

    WIP input()

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,109 @@
+use futures::executor::block_on;
+use winit::window::Window;
 use winit::{
     event::*,
-    event_loop::{ControlFlow,EventLoop},
+    event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
+
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+    async fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        // following instance to handle our GPU, BackendBit::Primary is for Metal/ Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                    label: None,
+                },
+                None,
+            )
+            .await
+            .unwrap();
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            format: adapter.get_swap_chain_preferred_format(&surface),
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+        Self {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+        todo!()
+    }
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+    fn update(&mut self) {
+        todo!()
+    }
+    fn render(&mut self) -> Result<(), wgpu::SwapChainError> {
+        todo!()
+    }
+}
 
 fn main() {
     env_logger::init();
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut state = block_on(State::new(&window));
 
-    event_loop.run(move | event,_,control_flow| match event {
-        Event::WindowEvent{ ref event,window_id,} 
-        if window_id == window.id() => match event {
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::Resized(physical_size) => {
+                state.resize(*physical_size);
+            }
+            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                state.resize(**new_inner_size);
+            }
             WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-            WindowEvent::KeyboardInput {input, ..} => match input {
-                KeyboardInput{
+            WindowEvent::KeyboardInput { input, .. } => match input {
+                KeyboardInput {
                     state: ElementState::Pressed,
                     virtual_keycode: Some(VirtualKeyCode::Escape),
                     ..
                 } => *control_flow = ControlFlow::Exit,
-                _=> {}
+                _ => {}
             },
             _ => {}
-        }
-        _ =>{}
+        },
+        _ => {}
     });
 }
