2c7348ff1b73c76957d636ec840020ee4c4d4f49
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-04-04 08:39:18 -0400

    Challenge Pipleline

--- src/main.rs
+++ src/main.rs
@@ -56,27 +56,6 @@ impl State {
 
         let vs_src = include_str!("shader.vert");
         let fs_src = include_str!("shader.frag");
-       /*  let mut compiler = shaderc::Compiler::new().unwrap();
-        let vs_spirv = compiler
-            .compile_into_spirv(
-                vs_src,
-                shaderc::ShaderKind::Vertex,
-                "shader.vert",
-                "main",
-                None,
-            )
-            .unwrap();
-        let fs_spirv = compiler
-            .compile_into_spirv(
-                fs_src,
-                shaderc::ShaderKind::Fragment,
-                "shader.frag",
-                "main",
-                None,
-            )
-            .unwrap();
-        let vs_data = wgpu::util::make_spirv(vs_spirv.as_binary_u8());
-        let fs_data = wgpu::util::make_spirv(fs_spirv.as_binary_u8()); */
         let vs_module = device.create_shader_module(&wgpu::include_spirv!("shader.vert.spv"));
         let fs_module = device.create_shader_module(&wgpu::include_spirv!("shader.frag.spv"));
         let render_pipleline_layout =
